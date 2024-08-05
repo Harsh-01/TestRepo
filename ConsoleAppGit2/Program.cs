26b118cf3858e2e1c1c742f13568b8e836faf726
Harsh-01 <adarsh9tiwari@gmail.com>
2024-08-05 15:36:26 +0530

    sec commit

--- ConsoleAppGit2/Program.cs
+++ ConsoleAppGit2/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Heyy Adarsh");
+
+Console.WriteLine("HEllo worls========");
+Console.WriteLine("Changes Made Here");
